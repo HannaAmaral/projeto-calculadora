ce78430676ffbd22efc7adf7aa416bdc4e0fcacf
Hanna Amaral <hannaap0001@gmail.com>
2025-08-05 22:06:06 -0300

    aviso

--- Calculadora.cs
+++ Calculadora.cs
@@ -13,7 +13,15 @@ namespace Calculadora
         }
 
         private void btnSomar_Click(object sender, EventArgs e)
+
         {
+            if (String.IsNullOrWhiteSpace(txtNumero1.Text) || String.IsNullOrWhiteSpace(txtNumero2.Text))
+            { 
+                MessageBox.Show("Preencha todos os números antes de calcular.");
+                return;
+            }
+
+
             double numero1 = Convert.ToDouble(txtNumero1.Text);
             double numero2 = Convert.ToDouble(txtNumero2.Text);
 
